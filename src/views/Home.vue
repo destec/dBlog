768b208a7386f605d23234b7de82763dd0da2910
destec <catalyst.zhang@gmail.com>
2018-08-08 19:38:34 +0800

    add basic components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+  <el-row class="full-height">
+    <el-col :span="4" class="menu">
+      <Menu></Menu>
+    </el-col>
+    <el-col :span="20">
+      <router-view></router-view>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from '@/components/Menu.vue';
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
-  }
+    Menu,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
 };
 </script>
+
+<style scoped>
+.full-height {
+  height: 100vh;
+}
+.menu {
+  height: 100vh;
+  background-color: #409eff;
+  text-align: center;
+}
+</style>
